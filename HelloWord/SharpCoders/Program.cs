5916558f5866ba1a8a3994bd5ce8968998c83181
Naty <Naty@DESKTOP-V88L10Q>
2022-11-22 09:48:50 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord/SharpCoders/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpCoders
+{
+
+    public class Program{
+
+        //Método main
+        public static void Main(String[] args){
+
+            //Printar algo na tela
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Primeira aula de C#");
+            Console.WriteLine("Testando o console write lime!");
+        }
+    }
+}
