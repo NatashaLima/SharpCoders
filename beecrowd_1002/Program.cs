5e0d2cab93b32e1ffecec7a53766c76df3fc75cb
Natasha Lima <100880246+NatashaLima@users.noreply.github.com>
2022-11-27 14:47:38 -0300

    Add files via upload

---
+++ beecrowd_1002/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+
+            raio = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = (3.14159 * raio * raio);
+
+            Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
